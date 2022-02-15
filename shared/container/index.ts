0fdc36b806c1b8818ccc78a3834d725eb5d43bae
Guilherme Augusto Rocha Sousa <guilhermesousadev@gmail.com>
2022-02-15 08:57:18 -0300

    Delete shared/container directory

--- shared/container/index.ts
+++
@@ -1,8 +0,0 @@
-import { IUserRepository } from '@modules/user/domain/repositories/IUserRepository';
-import { UserRepository } from '@modules/user/infra/typeorm/repositories/UserRepository';
-import { container } from 'tsyringe';
-
-container.registerSingleton<IUserRepository>(
-    'userRepository',
-    UserRepository,
-);
