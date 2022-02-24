8dca4deacaf235d198eb33192a3be3fc4066fbec
Guilherme Augusto Rocha Sousa <guilhermesousadev@gmail.com>
2022-02-24 20:06:57 -0300

    Receiveing file on upload page

--- frontend/src/pages/Upload/index.tsx
+++ frontend/src/pages/Upload/index.tsx
@@ -1,42 +1,43 @@
-import React, { FC, useState, useRef, MutableRefObject } from 'react';
+import React, { 
+    FC, 
+    useState, 
+    useRef, 
+    MutableRefObject 
+} from 'react';
 import { FiUpload } from 'react-icons/fi';
 import { Container, Text, UploadContainer } from './style';
+import { useDropzone } from 'react-dropzone';
 import Navbar from '../../components/Navbar';
 
 const Upload: FC = () => {
-    const inpFileRef = useRef() as MutableRefObject<HTMLInputElement>; 
+    const [file, setFile] = useState<File>();
     const [color, setColor] = useState<string>('#fff');
-
-    const handleFile = (e: any) => {
-        console.log(e.dataTransfer)
-        setColor('#fff');
-        console.log(inpFileRef);
-    };
-
-    const handleEnterFile = () => {
-        setColor('#ffbb00');
-    }
-
-    const handleLeaveFile = () => {
-        setColor('#fff');
-    }
-
-    const handleClickFile = () => inpFileRef.current.click();
-
+    const [msg, setMsg] = useState<string>('Arraste um Arquivo');
+    
+    const { getRootProps }  = useDropzone({
+        onDrop: acceptedFiles => {
+            setColor('#fff');
+            setFile(Object.assign(acceptedFiles[0], {
+                preview: URL.createObjectURL(acceptedFiles[0]),
+            }));
+        },
+        onDragEnter: () => setColor('#1a3365'),
+        onDragLeave: () => setColor('#fff'),
+        onDropAccepted: () => {
+            setMsg('Pronto para envio');
+            setColor('#28a745');
+        },
+    });
     return (
         <>
             <Navbar />
             <Container>
                 <UploadContainer 
+                    {...getRootProps()}
                     color={color}
-                    onDrop={handleFile}
-                    onDragEnter={handleEnterFile}
-                    onDragLeave={handleLeaveFile}
-                    onClick={handleClickFile}
                 >
                     <FiUpload />
-                    <Text>Clique ou Arraste o Arquivo</Text>
-                    <input ref={inpFileRef} type="file" />
+                    <Text>{msg}</Text>
                 </UploadContainer>
             </Container>
         </>
