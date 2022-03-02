3eb8030ba44266c138e6c27df0ef814d7c7b7787
Guilherme Augusto Rocha Sousa <guilhermesousadev@gmail.com>
2022-03-02 09:25:05 -0300

    Download File Page

--- frontend/src/pages/Files/ListUniqueFile.tsx
+++ frontend/src/pages/Files/ListUniqueFile.tsx
@@ -2,27 +2,20 @@ import React, { FC, useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Navbar from '../../components/Navbar';
 import api from '../../services/Axios';
-import { Container, Title } from './style';
-
-interface IUser {
-    id: number;
-    name: string;
-    email: string;
-    password: string;
-    avatar: string
-    createdAt: Date;
-    updatedAt: Date;
-}
+import { FiDownload } from 'react-icons/fi';
+import { Container, Title, Desc, Text, Button } from './style';
 
 interface IFile {
-    id: number
-    name: string
-    description: string
-    type: string
-    file: string
-    user: IUser;
-    createdAt: Date;
-    updatedAt: Date;
+    file: {
+        id: number;
+        name: string;
+        description: string;
+        file: string;
+        type: string;
+        createdAt: Date;
+        updatedAt: Date;
+    }
+    contentFile: string;
 }
 
 const ListUniqueFile: FC = () => {
@@ -31,9 +24,9 @@ const ListUniqueFile: FC = () => {
 
     useEffect(() => {
         (async () => {
-            const request = await api.get(`files/${id}`);
+            const { data } = await api.get<IFile>(`files/${id}`);
 
-            console.log(request);
+            setFile(data);
         })();
     }, [id]);
 
@@ -41,7 +34,18 @@ const ListUniqueFile: FC = () => {
         <>
             <Navbar />
             <Container>
-                
+                {file?
+                    <>
+                        <Title>{file.file.name}</Title>
+                        <Desc>{file.file.description}</Desc>
+                        <Text>Type: {file.file.type}</Text>
+                        <Text>{file.file.file}</Text>
+                        <a href={`download/http://localhost:8081/files/${file.file.file}`} download={file.file.file}>
+                            <Button>Download <FiDownload /></Button>
+                        </a>
+                    </>
+                    : '...Loading'
+                }
             </Container>
         </>
     )
--- frontend/src/pages/Files/style.ts
+++ frontend/src/pages/Files/style.ts
@@ -3,10 +3,45 @@ import styled from 'styled-components';
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
+    align-items: center;
+    height: 600px;
+    a {
+        text-decoration: none;
+    }
 `;
 
 export const Title = styled.h2`
     text-align: center;
     margin-bottom: 1rem;
     font-family: 'IBM Plex Sans', sans-serif;
+`;
+
+export const Desc = styled.span`
+    text-align: center;
+    margin-bottom: 1rem;
+    font-family: 'IBM Plex Sans', sans-serif;
+    font-size: 18px;
+`;
+
+export const Text = styled.span`
+    margin-bottom: 1rem;
+    font-family: 'IBM Plex Sans', sans-serif;
+`;
+
+export const Button = styled.button`
+    display: inline-block;
+    font-weight: 400;
+    text-align: center;
+    white-space: nowrap;
+    border: 1px solid transparent;
+    padding: 0.375rem 0.75rem;
+    font-size: 1rem;
+    line-height: 1.5;
+    border-radius: 0.25rem;
+    transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+    background-color: ${(props) => props.theme};
+    color: ${(props) => props.color};
+    border-color: #212529;
+    font-family: 'Montserrat', sans-serif;
+    cursor: pointer;
 `;
